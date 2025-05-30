c424718ae0a263d0c77485c23f984d3938d0a56a
Dhouha Gaoud <105125268+Dhouhaga@users.noreply.github.com>
2025-05-30 10:18:33 +0100

    Add files via upload

---
+++ CLI/src/common/GameConstants.java
@@ -0,0 +1,8 @@
+package common;
+
+public interface GameConstants {
+    int BOARD_SIZE = 3;
+    char EMPTY = ' ';
+    char PLAYER_X = 'X';
+    char PLAYER_O = 'O';
+}
---
+++ CLI/src/common/PlayerCallback.java
@@ -0,0 +1,13 @@
+// In common/PlayerCallback.java
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface PlayerCallback extends Remote {
+    void updateBoard(char[][] board) throws RemoteException;
+    void notifyTurn() throws RemoteException;
+    void gameOver(String message) throws RemoteException;
+    void displayMessage(String message) throws RemoteException;
+    void notifyNewGame() throws RemoteException; // Add this new method
+}
